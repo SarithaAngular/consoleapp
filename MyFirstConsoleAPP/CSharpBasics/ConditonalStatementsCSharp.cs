d23141da937df5c947dda20d8b1df635b6e7d880
SarithaAngular <gsaritha191919@gmail.com>
2022-11-29 11:27:19 +0530

    my fiirst

---
+++ MyFirstConsoleAPP/CSharpBasics/ConditonalStatementsCSharp.cs
@@ -0,0 +1,20 @@
+﻿  
+namespace CSharpBasics
+{
+      class ConditonalStatementsCSharp
+    {
+       static void Main()
+        {
+            //IF
+            int i = 10;
+            if(i > 20)
+            {
+                Console.WriteLine($" I is greater 20 {i} ");
+                Console.WriteLine("I is greater than 20 " + i);
+                Console.WriteLine("I am the old way {0}", i);
+
+            }
+            Console.WriteLine("Hello");
+        }
+    }
+}
